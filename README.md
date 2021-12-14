fad5e674c0aac397d51856d309f5c77479b02fe9
adnensaid <adnensaid83@gmail.com>
2021-12-14 17:26:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # composerQuest https://odyssey.wildcodeschool.com/quests/1144 
+# composer quete 2  https://odyssey.wildcodeschool.com/quests/1149
--- public/index.php
+++ public/index.php
@@ -1,12 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
-use App\Wcs\Hello;
 use CowSay\Cow;
-$sayHello = new Hello();
-echo $sayHello->talk();
 $bessie = new Cow('ciao tutti');
-
 // store the output in a variable
 $output = $bessie->say('ciao tutti !');
 echo $output;
